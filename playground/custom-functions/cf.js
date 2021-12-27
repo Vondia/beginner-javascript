268196e4535c313c6ea8e4ad57617f6588040265
Vondia <pimvdbosch@gmail.com>
2021-12-27 17:35:49 +0100

    done for today

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,12 @@
+// Function Definition
+function calculateBill() {
+        // This is the function body
+        console.log('Running Calculate Bill!!!');
+        const total = 100 * 1.13;
+        return total;
+}
+
+// Function call. or **run**
+const myTotal = calculateBill();
+
+console.log(`Your total is $${Math.ceil(myTotal)}`);
