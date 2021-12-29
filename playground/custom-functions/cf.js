96711ecd26423fe4601e928262b62da23552e977
Vondia <pimvdbosch@gmail.com>
2021-12-29 10:03:34 +0100

    whoop

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,12 +1,29 @@
 // Function Definition
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
         // This is the function body
-        console.log('Running Calculate Bill!!!');
-        const total = 100 * 1.13;
+        console.log('Running Calculate Bill!!!!');
+        const total = billAmount + billAmount * taxRate + billAmount * tipRate;
         return total;
 }
 
 // Function call. or **run**
-const myTotal = calculateBill();
+const myTotal = calculateBill(100, 0.13);
+const myTotal2 = calculateBill(200, 0.13);
+console.log(myTotal, myTotal2);
 
-console.log(`Your total is $${Math.ceil(myTotal)}`);
+function sayHiTo(firstName) {
+        return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo('Pim');
+// console.log(greeting);
+
+function doctorize(name) {
+        return `Dr. ${name}`;
+}
+
+function yell(name) {
+        return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
